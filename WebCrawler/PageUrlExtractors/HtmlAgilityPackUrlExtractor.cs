4959d851eb3366bde1693f8dea942e1732bf2449
Serhiy Buta <otaman963@gmail.com>
2018-03-14 20:22:11 +0200

    Added default url extractor (without implementation)

---
+++ WebCrawler/PageUrlExtractors/HtmlAgilityPackUrlExtractor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebCrawler.PageUrlExtractors
+{
+    internal class HtmlAgilityPackUrlExtractor : IPageUrlExtractor
+    {
+        public Task<IEnumerable<PageUrl>> ExtractAsync(PageUrl url, HttpResponseMessage response)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
