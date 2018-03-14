710cc90272519e97c7b45ecb2bbe8a2fd27e1e95
Serhiy Buta <otaman963@gmail.com>
2018-03-14 20:40:45 +0200

    Added implementation of default extractor

--- WebCrawler/PageUrlExtractors/HtmlAgilityPackUrlExtractor.cs
+++ WebCrawler/PageUrlExtractors/HtmlAgilityPackUrlExtractor.cs
@@ -1,14 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace WebCrawler.PageUrlExtractors
 {
     internal class HtmlAgilityPackUrlExtractor : IPageUrlExtractor
     {
-        public Task<IEnumerable<PageUrl>> ExtractAsync(PageUrl url, HttpResponseMessage response)
+        public async Task<IEnumerable<PageUrl>> ExtractAsync(PageUrl url, HttpResponseMessage response)
         {
-            throw new System.NotImplementedException();
+            var doc = new HtmlDocument();
+            doc.Load(await response.Content.ReadAsStreamAsync());
+            var links = doc.DocumentNode.Descendants("a")
+                .Select(a => a.GetAttributeValue("href", null))
+                .Where(u => !string.IsNullOrEmpty(u));
+            
+            var baseUrl = new Uri(url.CurrentUrl);
+            var authority = baseUrl.GetLeftPart(UriPartial.Authority);
+
+            return links.Select(link =>
+            {
+                var uri = new Uri(new Uri(authority), new Uri(link));
+                var preparedUrl = uri.ToString();
+                return new PageUrl(url.Depth + 1, url.CurrentUrl, preparedUrl);
+            });
         }
     }
 }
