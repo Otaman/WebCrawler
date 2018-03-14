173a17890ccfb789c3a9772c438a59f18225faab
Serhiy Buta <otaman963@gmail.com>
2018-03-14 20:20:37 +0200

    Added abstractions for url extraction/filtering, loaded page processing

---
+++ WebCrawler/IPageUrlFilter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    public interface IPageUrlFilter
+    {
+        Task<IEnumerable<PageUrl>> FilterAsync(IEnumerable<PageUrl> urls);
+    }
+}
