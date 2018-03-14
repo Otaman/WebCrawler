2e8ebd2cac8e262be0cdc86b1658d93ea7a70ae0
Serhiy Buta <otaman963@gmail.com>
2018-03-14 20:55:00 +0200

    Added filter for passing only unique urls

---
+++ WebCrawler/PageUrlFilters/UniquePageUrlsFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCrawler.PageUrlFilters
+{
+    public class UniquePageUrlsFilter : IPageUrlFilter
+    {
+        private ConcurrentDictionary<string, byte> _previousUrls = new ConcurrentDictionary<string, byte>();
+        
+        public async Task<IEnumerable<PageUrl>> FilterAsync(IEnumerable<PageUrl> urls)
+        {
+            return urls.Where(m => _previousUrls.TryAdd(m.CurrentUrl, 0));
+        }
+    }
+}
